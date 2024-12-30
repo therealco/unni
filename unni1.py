0b8596b6ee7e058f630d74e4deaac62f37b88cde
therealco <hawkinjames573@gmail.com>
2024-12-30 11:02:32 -0500

    Create unni1.py

---
+++ unni1.py
@@ -0,0 +1,18 @@
+import time
+print("Welcome!")
+conf1 = input("> ")
+str(P) in conf1 = 1
+str(A) in conf1 = 2
+str(V) in conf1 = 4
+str(N) in conf1 = 8
+t = 0
+if str(P) in conf1 True:
+  t = t + 1
+if str(A) in conf1 True:
+  t = t + 2
+if str(V) in conf1 True:
+  t = t + 4
+if str(N) in conf1 True:
+  t = t + 8
+time.sleep(3)
+print(int(t))
